8ba8621aecf82a0409a1266888c98d155b5aa747
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-03 22:52:00 -0500

    adding comments

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const ticketContainer = document.getElementById ('ticketList');
 const errorMessage = document.getElementById('errorMessage');
 async function displayTicket (UserId) {
-    try{
+    try{ // trying andthrowing api info for user id
         const UserIdResponse = await fetch (`https://jsonplaceholder.typicode.com/posts${UserId}`);
         if (!UserIdResponse.ok){
             throw new Error ('Failed to retrieve UserId');
@@ -17,11 +17,15 @@ async function displayTicket (UserId) {
         const ticketId = await ticketIdResponse.json();
         console.log ('TickedId:', ticketId);
         orders[0].products.forEach(product => {
-            const listItem = document.createElement('li');
-            listItem.textContent = `Customer Name: ${product, userid}, Issue Description: ${product.title}, Deatils: ${product.body}`;
+            const listItem = document.createElement('li'); //displaying ticket list
+            listItem.textContent = `Customer Name: ${product, userid}, Issue Description: ${product.title}, Details: ${product.body}`;
             orderList.appendChild (listItem);
         }); 
-    } catch (error){
+    } catch (error){ //catch error message
         console.error ('Error:', error.message);
+        //Task 4: Use finally to Ensure Cleanup
+    } finally {
+        
     }
 }
+displayTicket(1)
